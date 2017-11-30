c8b78ba640e2232bfd4fe30cbeb1a968683c01b7
Junwei Yuan <jimyuan1995@outlook.com>
2017-11-30 15:30:20 +0000

    add CLI

--- src/bgpd/bgpd.ml
+++ src/bgpd/bgpd.ml
@@ -1,5 +1,4 @@
 open Lwt.Infix
-open Printf
 
 module  Main (C: Mirage_console_lwt.S) (S: Mirage_stack_lwt.V4) = struct
   type flags = {
@@ -40,8 +39,9 @@ module  Main (C: Mirage_console_lwt.S) (S: Mirage_stack_lwt.V4) = struct
             | Bgp.Update u -> Fsm.Update_msg u
             | Bgp.Notification e -> Fsm.Notif_msg e
             | Bgp.Keepalive -> Fsm.Keepalive_msg
-            in              
-            Lwt.join [msg_read_loop t callback; callback event]
+            in
+            let _ = callback event in
+            msg_read_loop t callback
         end 
       | Error _ -> 
         let _ = C.log t.console "Debug: tcp read error in read_msg." in
@@ -69,7 +69,11 @@ module  Main (C: Mirage_console_lwt.S) (S: Mirage_stack_lwt.V4) = struct
           t.flow <- Some flow;
           t.flags.read_tcp_flow <- true;
           let _ = C.log t.console "Debug: connected to remote." in
-          Lwt.join [callback Fsm.Tcp_CR_Acked; msg_read_loop t callback]
+
+          (* Spawn a thread to read messages from flow *)
+          let _ = msg_read_loop t callback in
+
+          callback Fsm.Tcp_CR_Acked
         end
         | Some _ -> S.TCPV4.close flow
       end
@@ -81,7 +85,12 @@ module  Main (C: Mirage_console_lwt.S) (S: Mirage_stack_lwt.V4) = struct
       if (t.flags.listen_tcp_conn) then begin
         t.flow <- Some flow;
         t.flags.read_tcp_flow <- true;
-        Lwt.join [callback Fsm.Tcp_connection_confirmed; msg_read_loop t callback]
+        let _ = C.log t.console "Debug: accept remote connection." in
+
+        (* Spawn a thread to read messages from flow *)
+        let _ = msg_read_loop t callback in
+
+        callback Fsm.Tcp_connection_confirmed
       end
       else Lwt.return_unit (* Silently quit *)
     in
@@ -128,28 +137,49 @@ module  Main (C: Mirage_console_lwt.S) (S: Mirage_stack_lwt.V4) = struct
     let open Fsm in
     let callback = fun event -> handle_event t event in
     match action with
-    | Initiate_tcp_connection ->
-      init_tcp_connection t callback
+    | Initiate_tcp_connection -> init_tcp_connection t callback
     | Send_open_msg -> send_open_msg t
     | Send_msg msg -> send_msg t msg
     | Drop_tcp_connection -> drop_tcp_connection t
     | Listen_tcp_connection -> t.flags.listen_tcp_conn <- true; Lwt.return_unit
     | _ -> Lwt.return_unit
+
   and handle_event t event =
-    let open Fsm in
-    let new_fsm, actions = handle t.fsm event in
+    let new_fsm, actions = Fsm.handle t.fsm event in
     t.fsm <- new_fsm;
-    Lwt.join (List.map (perform_action t) actions)
+    (* Spawn threads to perform actions from left to right *)
+    let _ = List.fold_left (fun acc act -> List.cons (perform_action t act) acc) [] actions in
+    Lwt.return_unit
   ;;
+
 
   let start_bgp host_id my_id my_as socket console =
     let fsm = Fsm.create 120 90 30 in
     let flags = { listen_tcp_conn = false; read_tcp_flow = false } in
     let flow = None in
     let t = { host_id; my_id; my_as; socket; console; fsm; flow; flags; } in
+
+    (* Start listening to BGP port. *)
     listen_tcp_connection t (handle_event t);
-    let _ = C.log console "Debug: bgpd start." in
-    handle_event t (Fsm.Manual_start)
+
+    (* This is the command loop *)
+    let rec loop () =
+      Lwt_io.read_line Lwt_io.stdin
+      >>= function
+      | "start" -> 
+        let _ = C.log console "Debug: bgpd starts." in
+        handle_event t (Fsm.Manual_start) >>= loop
+      | "stop" -> 
+        let _ = C.log console "Debug: bgpd stops." in
+        handle_event t (Fsm.Manual_stop) >>= loop
+      | "exit" -> 
+        let _ = C.log console "Debug: exiting." in
+        S.disconnect socket
+        >>= fun () ->
+        Lwt.return_unit
+      | _ -> Lwt.return_unit >>= loop
+    in
+    loop ()
   ;;
 
   let start c s =
