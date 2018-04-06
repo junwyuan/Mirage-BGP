a84fb284992c5b60dc7a0511db4d5ab03457f421
Junwei Yuan <jimyuan1995@outlook.com>
2018-04-06 21:28:58 +0100

    change test

--- src/perf/config.ml
+++ src/perf/config.ml
@@ -10,7 +10,42 @@ let start_time =
     Key.Arg.info ~doc:"This is the time to wait after connection establishment before start the test."
     ["start_time"]
   in
-  Key.(create "start_time" Arg.(opt int 5 doc))
+  Key.(create "start_time" Arg.(opt int 3 doc))
+;;
+
+let test = 
+  let doc = 
+    Key.Arg.info ~doc:"Choose what perf to perform."
+    ["test"]
+  in
+  Key.(create "test" Arg.(opt string "insert phased throughput" doc))
+
+let pfx_per_msg =
+  let doc = 
+    Key.Arg.info ~doc:"Number of prefixes per message." ["p"]
+  in
+  Key.(create "pfx_per_msg" Arg.(opt int 100 doc))
+;;
+
+let msg_per_round = 
+  let doc = 
+    Key.Arg.info ~doc:"Number of messages sent per rond." ["m"]
+  in
+  Key.(create "msg_per_round" Arg.(opt int 500 doc))
+;;
+
+let repeat =
+  let doc = 
+    Key.Arg.info ~doc:"Number of time the test should be repeated" ["r"]
+  in
+  Key.(create "repeat_times" Arg.(opt int 1 doc))
+;;
+
+let verbose =
+  let doc = 
+    Key.Arg.info ~doc:"Decompile." ["d"]
+  in
+  Key.(create "decompile" Arg.(flag doc))
 ;;
 
 
@@ -18,6 +53,11 @@ let main =
   let keys = [ 
     Key.abstract config;
     Key.abstract start_time;
+    Key.abstract test;
+    Key.abstract pfx_per_msg;
+    Key.abstract msg_per_round;
+    Key.abstract verbose;
+    Key.abstract repeat;
   ] in
   foreign ~keys "Perf.Main" (stackv4 @-> job)
 ;;
--- src/perf/perf.ml
+++ src/perf/perf.ml
@@ -21,7 +21,7 @@ module Main (S: Mirage_stack_lwt.V4) = struct
       local_id = relay.local_id;
       local_asn = relay.local_asn;
       options = [ Capability [Mp_ext (Afi.IP4, Safi.UNICAST)] ];
-      hold_time = 180;
+      hold_time = 720;
     } in
     Bgp.Open o
   ;;
@@ -70,6 +70,7 @@ module Main (S: Mirage_stack_lwt.V4) = struct
               | Ok msg ->
                 match msg with
                 | Keepalive ->
+                  OS.Time.sleep_ns (Duration.of_ms 500) >>= fun () ->
                   Lwt.return (flow, o)
                 | _ -> 
                   Log.err (fun m -> m "wrong msg type: %s" (Bgp.to_string msg));
@@ -85,14 +86,24 @@ module Main (S: Mirage_stack_lwt.V4) = struct
       Bgp_flow.close flow
   ;;
 
-  let plain_feed ?(num_pfx_per_msg=500) ~total_num_msg ~seed ~flow ~path_attrs (module Log : Logs.LOG) =
-    let rec loop count seed = 
-      if count >= total_num_msg then Lwt.return seed
+  let plain_feed ~pfx_per_msg ~asn ~num_msg ~seed ~flow ~(peer:Config_parser.relay) ~path_attrs (module Log : Logs.LOG) =
+    let rec loop count seed asn_count = 
+      if count >= num_msg then Lwt.return (seed, asn_count)
       else 
-        let nlri, n_seed = Pfx_gen.gen seed num_pfx_per_msg in
+        let nlri, n_seed = Pfx_gen.gen seed pfx_per_msg in
+
+        let path_attrs = 
+          let origin = Origin EGP in
+          let next_hop = Next_hop peer.local_id in
+          let as_path = As_path [Asn_seq [peer.local_asn; asn_count; 65001_l; 65002_l; 65003_l]] in
+          [origin; next_hop; as_path]
+        in
+
         let msg = Update { withdrawn=[]; path_attrs; nlri } in
 
         Log.debug (fun m -> m "Feed %d: %s" count (Bgp.to_string msg));
+
+        let asn_count = if asn_count > 65000_l then 3000_l else Int32.add asn_count 1_l in
         
         Bgp_flow.write flow msg
         >>= function
@@ -100,17 +111,17 @@ module Main (S: Mirage_stack_lwt.V4) = struct
           Log.err (fun m -> m "fail to write");
           Lwt.fail_with "fail to write"
         | Ok () -> 
-          loop (count + 1) n_seed
+          loop (count + 1) n_seed asn_count
     in
-    loop 0 seed
+    loop 0 seed asn
   ;;
 
 
-  let plain_feed_withdrawl ?(num_pfx_per_msg=500) ~total_num_msg ~seed ~flow (module Log : Logs.LOG) =
+  let plain_feed_withdrawl ~pfx_per_msg ~num_msg ~seed ~flow (module Log : Logs.LOG) =
     let rec loop count seed = 
-      if count >= total_num_msg then Lwt.return seed
+      if count >= num_msg then Lwt.return seed
       else
-        let withdrawn, n_seed = Pfx_gen.gen seed num_pfx_per_msg in
+        let withdrawn, n_seed = Pfx_gen.gen seed pfx_per_msg in
         let msg = Update { withdrawn; path_attrs = []; nlri= [] } in
 
         Log.debug (fun m -> m "Feed %d: %s" count (Bgp.to_string msg));
@@ -203,7 +214,7 @@ module Main (S: Mirage_stack_lwt.V4) = struct
 
   let rec write_keepalive_loop flow (module Log : Logs.LOG) =
     let rec loop () = 
-      let%lwt () = OS.Time.sleep_ns (Duration.of_sec 30) in
+      let%lwt () = OS.Time.sleep_ns (Duration.of_sec 60) in
 
       match%lwt Bgp_flow.write flow Bgp.Keepalive with
       | Error _ ->
@@ -216,7 +227,7 @@ module Main (S: Mirage_stack_lwt.V4) = struct
     loop ()
   ;;
 
-  let phased_insert ?(num_pfx_per_msg=500) ?(num_pfx_per_round=100000) ~phase_name ~total_num_pfx ~seed ~path_attrs ~flow1 ~flow2 (module Log1 : Logs.LOG) (module Log2 : Logs.LOG) =
+  (* let phased_insert ?(pfx_per_msg=500) ?(num_pfx_per_round=100000) ~phase_name ~total_num_pfx ~seed ~path_attrs ~peer ~flow1 ~flow2 (module Log1 : Logs.LOG) (module Log2 : Logs.LOG) =
     let num_stages = total_num_pfx / num_pfx_per_round in
     let rec loop stage_count seed time = 
       if stage_count = num_stages then begin
@@ -226,11 +237,11 @@ module Main (S: Mirage_stack_lwt.V4) = struct
       else begin
         (* Latency test *)
         (* Listen speaker2 *)
-        let rec_rloop = read_loop_count_nlri flow2 num_pfx_per_msg (module Log2) in
+        let rec_rloop = read_loop_count_nlri flow2 pfx_per_msg (module Log2) in
         (* Ping *)
         let start_time = Unix.gettimeofday () in
         
-        plain_feed ~num_pfx_per_msg ~total_num_msg:1 ~flow:flow1 ~seed ~path_attrs (module Log1)
+        plain_feed ~pfx_per_msg ~num_msg:1 ~flow:flow1 ~peer ~seed ~path_attrs (module Log1)
         >>= fun n_seed ->
         (* Speaker2 checks on receiving all updates *)
         rec_rloop
@@ -242,10 +253,10 @@ module Main (S: Mirage_stack_lwt.V4) = struct
 
         (* Throughput test *)
         (* Listen speaker2 *)
-        let rec_rloop = read_loop_count_nlri flow2 (num_pfx_per_round - num_pfx_per_msg) (module Log2) in
+        let rec_rloop = read_loop_count_nlri flow2 (num_pfx_per_round - pfx_per_msg) (module Log2) in
         (* start feeding speaker1 *)
         let start_time = Unix.gettimeofday () in
-        plain_feed ~num_pfx_per_msg ~total_num_msg:(num_pfx_per_round / num_pfx_per_msg - 1) ~flow:flow1 ~seed:n_seed ~path_attrs (module Log1)
+        plain_feed ~pfx_per_msg ~num_msg:(num_pfx_per_round / pfx_per_msg - 1) ~peer ~flow:flow1 ~seed:n_seed ~path_attrs (module Log1)
         >>= fun n_seed ->
         (* Speaker2 checks on receiving all updates *)
         rec_rloop 
@@ -265,7 +276,7 @@ module Main (S: Mirage_stack_lwt.V4) = struct
     loop 0 seed 0.
   ;;
 
-  let phased_withdrawn ?(num_pfx_per_msg=500) ?(num_pfx_per_round=100000) ~total_num_pfx ~seed ~flow1 ~flow2 (module Log1 : Logs.LOG) (module Log2 : Logs.LOG) =
+  let phased_withdrawn ?(pfx_per_msg=500) ?(num_pfx_per_round=100000) ~total_num_pfx ~seed ~flow1 ~flow2 (module Log1 : Logs.LOG) (module Log2 : Logs.LOG) =
     let num_stages = total_num_pfx / num_pfx_per_round in
     let rec loop stage_count seed time = 
       if stage_count = num_stages then begin
@@ -275,11 +286,11 @@ module Main (S: Mirage_stack_lwt.V4) = struct
       else begin
         (* Latency test *)
         (* Listen speaker2 *)
-        let rec_rloop = read_loop_count_withdrawn flow2 num_pfx_per_msg (module Log2) in
+        let rec_rloop = read_loop_count_withdrawn flow2 pfx_per_msg (module Log2) in
         (* Ping *)
         let start_time = Unix.gettimeofday () in
         
-        plain_feed_withdrawl ~num_pfx_per_msg:num_pfx_per_msg ~total_num_msg:1 ~flow:flow1 ~seed (module Log1)
+        plain_feed_withdrawl ~pfx_per_msg:pfx_per_msg ~num_msg:1 ~flow:flow1 ~seed (module Log1)
         >>= fun n_seed ->
         (* Speaker2 checks on receiving all updates *)
         rec_rloop
@@ -291,10 +302,10 @@ module Main (S: Mirage_stack_lwt.V4) = struct
 
         (* Throughput test *)
         (* Listen speaker2 *)
-        let rec_rloop = read_loop_count_withdrawn flow2 (num_pfx_per_round - num_pfx_per_msg) (module Log2) in
+        let rec_rloop = read_loop_count_withdrawn flow2 (num_pfx_per_round - pfx_per_msg) (module Log2) in
         (* start feeding speaker1 *)
         let start_time = Unix.gettimeofday () in
-        plain_feed_withdrawl ~total_num_msg:(num_pfx_per_round / num_pfx_per_msg - 1) ~num_pfx_per_msg ~flow:flow1 ~seed:n_seed (module Log1)
+        plain_feed_withdrawl ~num_msg:(num_pfx_per_round / pfx_per_msg - 1) ~pfx_per_msg ~flow:flow1 ~seed:n_seed (module Log1)
         >>= fun n_seed ->
         (* Speaker2 checks on receiving all updates *)
         rec_rloop 
@@ -314,7 +325,7 @@ module Main (S: Mirage_stack_lwt.V4) = struct
     loop 0 seed 0.
   ;;
   
-  let start_perf_test ?(num_pfx_per_msg=500) ?(num_pfx_per_round=50000) ~s ~total_num_pfx ~seed ~(config:Config_parser.config) =
+  let start_perf_test ?(pfx_per_msg=500) ?(num_pfx_per_round=50000) ~s ~total_num_pfx ~seed ~(config:Config_parser.config) =
     let peer1 = List.nth config.relays 0 in
     let peer2 = List.nth config.relays 1 in
 
@@ -341,7 +352,7 @@ module Main (S: Mirage_stack_lwt.V4) = struct
       let as_path = As_path [Asn_seq [peer1.local_asn; 2000_l; 2001_l; 2002_l; 2003_l]] in
       [origin; next_hop; as_path]
     in
-    phased_insert ~num_pfx_per_round ~num_pfx_per_msg ~seed:(Pfx_gen.default_seed) ~total_num_pfx ~flow1 ~flow2 
+    phased_insert ~num_pfx_per_round ~pfx_per_msg ~peer:peer1 ~seed:(Pfx_gen.default_seed) ~total_num_pfx ~flow1 ~flow2 
                   ~path_attrs (module Sp1_log : Logs.LOG) (module Sp2_log : Logs.LOG) ~phase_name:"Insert"
     >>= fun n_seed ->
 
@@ -355,7 +366,7 @@ module Main (S: Mirage_stack_lwt.V4) = struct
         [origin; next_hop; as_path]
       in
       let start_time = Unix.gettimeofday () in
-      plain_feed  ~num_pfx_per_msg ~total_num_msg:(num_pfx_per_round / num_pfx_per_msg) ~seed:Pfx_gen.default_seed ~path_attrs
+      plain_feed  ~pfx_per_msg ~num_msg:(num_pfx_per_round / pfx_per_msg) ~peer:peer1 ~seed:Pfx_gen.default_seed ~path_attrs
                   ~flow:flow2 (module Sp2_log : Logs.LOG)
       >>= fun _ ->
       let update = {
@@ -383,15 +394,15 @@ module Main (S: Mirage_stack_lwt.V4) = struct
       let as_path = As_path [Asn_seq [peer1.local_asn; 2004_l; 2005_l; 2006_l]] in
       [origin; next_hop; as_path]
     in
-    phased_insert ~num_pfx_per_round ~num_pfx_per_msg ~path_attrs
+    phased_insert ~num_pfx_per_round ~pfx_per_msg ~path_attrs
                   ~seed:(Pfx_gen.default_seed) ~total_num_pfx:(total_num_pfx/2) 
-                  ~flow1 ~flow2 
+                  ~flow1 ~flow2 ~peer:peer1
                   (module Sp1_log : Logs.LOG) (module Sp2_log : Logs.LOG) 
                   ~phase_name:"replace"
     >>= fun _ ->
 
     (* Withdrawn phase *)
-    phased_withdrawn  ~num_pfx_per_round ~num_pfx_per_msg  ~total_num_pfx:(total_num_pfx/2) 
+    phased_withdrawn  ~num_pfx_per_round ~pfx_per_msg  ~total_num_pfx:(total_num_pfx/2) 
                       ~seed:(Pfx_gen.default_seed) 
                       ~flow1 ~flow2
                       (module Sp1_log : Logs.LOG) (module Sp2_log : Logs.LOG)
@@ -422,16 +433,214 @@ module Main (S: Mirage_stack_lwt.V4) = struct
     >>= fun () ->
 
     Lwt.return seed
+  ;; *)
+
+  module Math = struct
+    let mean l = 
+      let sum = List.fold_left (fun acc x -> acc +. x) 0. l in
+      sum /. float_of_int (List.length l)
+    ;;
+
+    let std l =
+      let mean = mean l in
+      let sum_sq_err = List.fold_left (fun acc x -> acc +. (x -. mean) *. (x -. mean)) 0. l in
+      sqrt (sum_sq_err /. float_of_int (List.length l))
+    ;;
+  end
+
+  let insert_phased_throughput_test ~num_round ~round_gap ~msg_per_round ~pfx_per_msg ~socket ~(config:Config_parser.config) () =
+    let peer1 = List.nth config.relays 0 in
+    let peer2 = List.nth config.relays 1 in
+
+    (* connect to speaker1 *)
+    create_session socket peer1 (module Sp1_log)
+    >>= fun (flow1, _) ->
+    (* connect to speaker2 *)
+    create_session socket peer2 (module Sp2_log)
+    >>= fun (flow2, _) ->
+
+    Mon_log.info (fun m -> m "Connection up.");
+
+    (* Keepalive loop *)
+    let _ = write_keepalive_loop flow2 (module Sp2_log) in
+
+    (* Wait through the start up mechanism *)
+    OS.Time.sleep_ns (Duration.of_sec (Key_gen.start_time ()))
+    >>= fun () ->
+
+    let path_attrs = 
+      let origin = Origin EGP in
+      let next_hop = Next_hop peer1.local_id in
+      let as_path = As_path [Asn_seq [peer1.local_asn; 65000_l; 65001_l; 65002_l; 65003_l]] in
+      [origin; next_hop; as_path]
+    in
+
+    let pfx_per_round = pfx_per_msg * msg_per_round in
+
+    let rec loop round_count seed asn results = 
+      if round_count = num_round then Lwt.return results
+      else
+        let rec repeat count results : float list Lwt.t =
+          if count > 0 then 
+            let rec_rloop = read_loop_count_nlri flow2 pfx_per_round (module Sp2_log) in
+            let start_time = Unix.gettimeofday () in          
+            plain_feed  ~flow:flow1 
+                        ~pfx_per_msg 
+                        ~num_msg:msg_per_round
+                        ~path_attrs
+                        ~seed
+                        ~asn
+                        ~peer: peer1
+                        (module Sp1_log : Logs.LOG)
+            >>= fun _ ->
+            rec_rloop >>= fun () ->
+            let end_time = Unix.gettimeofday () in
+            let throughput = (float_of_int pfx_per_round) /. (end_time -. start_time) in
+            if Key_gen.decompile () then
+              Mon_log.info (fun m -> m "Phased insert throughput: RIB size %d, time: %.4fs, throughput: %.2f per second." 
+                                      (round_count * round_gap) (end_time -. start_time) throughput);
+
+            let wd_rloop = read_loop_count_withdrawn flow2 pfx_per_round (module Sp2_log) in
+            plain_feed_withdrawl  ~pfx_per_msg
+                                  ~flow:flow1
+                                  ~num_msg: msg_per_round
+                                  ~seed
+                                  (module Sp1_log : Logs.LOG)
+            >>= fun _ -> 
+            wd_rloop >>= fun () ->
+
+            (* OS.Time.sleep_ns (Duration.of_sec 3) >>= fun () -> *)
+            
+            repeat (count - 1) ((end_time -. start_time)::results)
+          else Lwt.return results
+        in
+        repeat (Key_gen.repeat_times ()) [] >>= fun times ->
+
+        let mean, std = Math.mean times, Math.std times in
+        let throughput = (float_of_int pfx_per_round) /. mean in
+        let err = throughput *. (std /. mean) in
+
+        Mon_log.info (fun m -> m "%d, %f, %f, %f, %f" (round_count * round_gap) mean std throughput err);
+        
+        let rec_rloop = read_loop_count_nlri flow2 round_gap (module Sp2_log) in
+        plain_feed  ~flow:flow1 
+                    ~pfx_per_msg:1000
+                    ~num_msg: (round_gap / 1000)
+                    ~path_attrs
+                    ~asn
+                    ~seed:seed
+                    ~peer:peer1
+                    (module Sp1_log : Logs.LOG)
+        >>= fun (pfx_seed, new_asn) ->
+        rec_rloop >>= fun () ->
+
+        OS.Time.sleep_ns (Duration.of_sec 3) >>= fun () ->
+        
+        loop (round_count + 1) pfx_seed new_asn (((round_count * round_gap), (mean, std, throughput, err))::results)
+    in
+    loop 0 Pfx_gen.default_seed 3000_l [] >>= fun results ->
+
+    let data = List.map (fun (rib_size, (mean, std, throughput, err)) -> Printf.sprintf "%d, %f, %f, %f, %f" rib_size mean std throughput err) (List.rev results) in
+    Mon_log.info (fun m -> m "Phased insert throughput: %s" (String.concat "\n" data));
+
+    (* Close flows *)
+    close_session flow1 (module Sp1_log)
+    >>= fun () ->
+    close_session flow2 (module Sp2_log)
+    >>= fun () ->
+
+    Lwt.return_unit
   ;;
 
+  let next (x, y) =
+    if y >= 65000 then (x+1, 2500)
+    else (x, y+1)
+  ;; 
+
+
+  (* let insert_throughput_test ~num_msg ~pfx_per_msg ~socket ~(config:Config_parser.config) () =
+    let peer1 = List.nth config.relays 0 in
+    let peer2 = List.nth config.relays 1 in
+
+    (* connect to speaker1 *)
+    create_session socket peer1 (module Sp1_log)
+    >>= fun (flow1, _) ->
+    (* connect to speaker2 *)
+    create_session socket peer2 (module Sp2_log)
+    >>= fun (flow2, _) ->
+
+    Mon_log.info (fun m -> m "Connection up.");
+
+    (* Keepalive loop *)
+    let _ = write_keepalive_loop flow2 (module Sp2_log) in
+
+    (* Wait through the start up mechanism *)
+    OS.Time.sleep_ns (Duration.of_sec (Key_gen.start_time ()))
+    >>= fun () ->
+
+    let total = pfx_per_msg * num_msg in
+
+    let rec_rloop = read_loop_count_nlri flow2 total (module Sp2_log) in
+
+    let start_time = Unix.gettimeofday () in
+
+    plain_feed  ~flow:flow1 
+                ~pfx_per_msg 
+                ~num_msg
+                ~path_attrs:[]
+                ~peer:peer1
+                ~seed:Pfx_gen.default_seed
+                (module Sp1_log : Logs.LOG)
+    >>= fun pfx_seed ->
+    rec_rloop >>= fun () ->
+
+    let end_time = Unix.gettimeofday () in
+    
+    let throughput = (float_of_int total) /. (end_time -. start_time) in
+        
+    if Key_gen.decompile () then
+      Mon_log.info (fun m -> m "Insert throughput: test size %d, time: %.4fs, throughput: %.2f per second." 
+                              total (end_time -. start_time) throughput);
+    
+    (* Close flows *)
+    close_session flow1 (module Sp1_log)
+    >>= fun () ->
+    close_session flow2 (module Sp2_log)
+    >>= fun () ->
+
+    Lwt.return_unit
+  ;; *)
+
+  let rec run tests = 
+    let interval = 15 in
+    match tests with
+    | test::other -> 
+      test () >>= fun () ->
+      if other <> [] then 
+        OS.Time.sleep_ns (Duration.of_sec interval) >>= fun () ->
+        run other
+      else Lwt.return_unit
+    | [] -> Lwt.return_unit
+  ;;
   
-  let start s =
+  let start socket =
     let config = Config_parser.parse_from_file (Key_gen.config ()) in
-    let total_num_pfx = 200000 in
-    let seed = Pfx_gen.default_seed in
+
     Mon_log.info (fun m -> m "Test starts.");
-    start_perf_test ~num_pfx_per_msg:500 ~num_pfx_per_round:50000 ~s ~total_num_pfx ~seed ~config
-    >>= fun new_seed ->
+    let tests = [
+      insert_phased_throughput_test ~num_round:10
+                                    ~round_gap:200000
+                                    ~pfx_per_msg:(Key_gen.pfx_per_msg ())
+                                    ~msg_per_round:(Key_gen.msg_per_round ())
+                                    ~socket
+                                    ~config;
+      (* insert_throughput_test  ~pfx_per_msg:(Key_gen.pfx_per_msg ())
+                              ~num_msg:(Key_gen.msg_per_round ())
+                              ~socket
+                              ~config; *)
+    ] in
+    run tests >>= fun () ->
+
     Mon_log.info (fun m -> m "Test finishes.");
     Lwt.return_unit
   ;;
