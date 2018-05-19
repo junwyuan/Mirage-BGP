91470403c6069f96051d0f3514bb6a84682d20e7
Junwei Yuan <jimyuan1995@outlook.com>
2018-05-19 10:22:51 +0100

    add back some old test

---
+++ src/receiver/receive.ml
@@ -0,0 +1,167 @@
+open Lwt.Infix
+open Printf
+open Bgp
+
+let rec_log = Logs.Src.create "receiver" ~doc:"Receiver log"
+module Rec_log = (val Logs.src_log rec_log : Logs.LOG)
+
+module  Main (S: Mirage_stack_lwt.V4) = struct
+  module Bgp_flow = Bgp_io.Make(S)
+
+  let start_time = Unix.gettimeofday ();;
+  let time f c =
+    let t = Sys.time () in
+    f () >>= fun () ->
+    Rec_log.info (fun m -> m "execution time: %fs\n" (Sys.time() -. t));
+    Lwt.return_unit
+  ;;
+  let count = ref 0
+
+  let read_tcp_base flow callback = 
+    Bgp_flow.read flow
+    >>= fun read_result ->
+    match read_result with 
+    | Ok msg -> 
+      count := !count + 1;
+      Rec_log.info (fun m -> m "Receive pkg %d: %s" (!count) (Bgp.to_string msg));
+      callback ()
+    | Error err ->
+      (match err with 
+      | `Refused -> Rec_log.debug (fun m -> m "Read refused")
+      | `Timeout -> Rec_log.debug (fun m -> m "Read timeout")
+      | `Closed -> Rec_log.debug (fun m -> m "Connection closed when read.")
+      | `BGP_MSG_ERR err ->
+        (match err with
+        | Bgp.Parsing_error -> Rec_log.debug (fun m -> m "Parsing error")
+        | _ -> Rec_log.debug (fun m -> m "Msg format error"))
+      | _ -> ()); 
+      Lwt.return_unit
+  ;;
+
+  let read_flow_once flow = 
+    read_tcp_base flow (fun () -> Lwt.return_unit)
+  ;;
+
+  let rec read_flow_loop flow = 
+    read_tcp_base flow (fun () -> read_flow_loop flow)
+  ;;
+
+  let write_msg flow = fun msg ->
+    Rec_log.info (fun m -> m "send TCP message %s" (Bgp.to_string msg));
+    Bgp_flow.write flow msg 
+    >>= function
+    | Error err ->
+      (match err with
+      | `Timeout -> Rec_log.info (fun m -> m "Write time out")
+      | `Refused -> Rec_log.info (fun m -> m "Write refused.")
+      | `Closed -> Rec_log.info (fun m -> m "Connection closed when write.") 
+      | _ -> ());
+      Lwt.fail_with "TCP FAIL"
+    | Ok _ -> Lwt.return_unit
+  ;;
+
+  let rec write_keepalive flow =
+    OS.Time.sleep_ns (Duration.of_sec 30) 
+    >>= fun () ->
+    write_msg flow Bgp.Keepalive
+    >>= fun () ->
+    write_keepalive flow
+  ;;
+
+  let ip4_of_ints a b c d =
+    Int32.of_int ((a lsl 24) lor (b lsl 16) lor (c lsl 8) lor d)
+  ;;
+  
+  let write_update flow =
+    let open Bgp in
+    let withdrawn = 
+      [(Afi.IPv4 (ip4_of_ints 192 168 0 0), 16); 
+        (Afi.IPv4 (ip4_of_ints 10 0 0 0), 8); 
+        (Afi.IPv4 (ip4_of_ints 172 16 84 0), 24);
+        ] 
+    in
+    let nlri = [(Afi.IPv4 (ip4_of_ints 192 168 0 0), 24)] in
+    let flags = {optional=false; transitive=true; partial=false; extlen=false} in
+    let path_attrs = [
+      flags, Origin IGP;
+      flags, As_path [Set [2_l; 5_l; 3_l]; Seq [10_l; 20_l; 30_l]];
+      flags, Next_hop (Ipaddr.V4.to_int32 (Ipaddr.V4.of_string_exn (Key_gen.local_id ())));
+    ] in 
+    let u = Update {withdrawn; path_attrs; nlri} in
+    write_msg flow u
+  ;;
+
+  let rec loop () = 
+    OS.Time.sleep_ns (Duration.of_sec 60) 
+    >>= fun () ->
+    loop ()
+  ;;
+
+  let start_receive_passive flow =
+    Rec_log.info (fun m -> m "Accept incoming connection from remote.");
+    read_flow_once flow
+    >>= fun () ->
+    let open Bgp in
+    let o = {
+      version = 4;
+      bgp_id = Ipaddr.V4.to_int32 (Ipaddr.V4.of_string_exn (Key_gen.local_id ()));
+      my_as = Asn (Key_gen.local_asn ());
+      options = [];
+      hold_time = 180;
+    } in
+    write_msg flow (Bgp.Open o)
+    >>= fun () ->
+    read_flow_once flow
+    >>= fun () ->
+    write_msg flow (Bgp.Keepalive)
+    >>= fun () ->
+    Lwt.join [read_flow_loop flow; write_keepalive flow]
+  ;;
+
+  let start_receive_active flow =
+    let open Bgp in
+    let o = {
+      version = 4;
+      bgp_id = Ipaddr.V4.to_int32 (Ipaddr.V4.of_string_exn (Key_gen.local_id ()));
+      my_as = Asn (Key_gen.local_asn ());
+      options = [];
+      hold_time = 180;
+    } in
+    write_msg flow (Bgp.Open o)
+    >>= fun () ->
+    read_flow_once flow 
+    >>= fun () ->
+    write_msg flow (Bgp.Keepalive)
+    >>= fun () ->
+    read_flow_once flow
+    >>= fun () ->
+    Lwt.join [
+      Lwt.catch (fun () -> read_flow_loop flow) (fun exn -> Rec_log.err (fun m -> m "Some exn catched"); Lwt.return_unit);
+      write_keepalive flow
+    ]
+  ;;
+
+  let start s =
+    let port = Key_gen.local_port () in
+    Bgp_flow.listen s port (fun flow -> start_receive_passive flow);
+
+    let _ = 
+      Bgp_flow.create_connection s (Ipaddr.V4.of_string_exn (Key_gen.remote_id ()), Key_gen.remote_port ())
+      >>= function
+      | Error err -> 
+        (match err with
+        | `Timeout -> Rec_log.info (fun m -> m "Conn timeout")
+        | `Refused -> Rec_log.info (fun m -> m "Conn refused")
+        | _ -> ());
+        Lwt.return_unit
+      | Ok flow -> 
+        Rec_log.info (fun m -> m "Connect to remote"); 
+        start_receive_active flow
+    in
+
+    loop ()
+  ;;
+end
+
+
+
---
+++ src/replay/config.ml
@@ -0,0 +1,63 @@
+open Mirage
+
+let remote_id = 
+  let doc = Key.Arg.info ~doc:"This is the remote bgp speaker's ip address." ["remote_id"] in
+  Key.(create "remote_id" Arg.(opt string "172.19.0.2" doc))
+;;
+
+let remote_port = 
+  let doc = Key.Arg.info ~doc:"This is the remote bgp speaker's port." ["remote_port"] in
+  Key.(create "remote_port" Arg.(opt int 179 doc))
+;;
+
+let local_id = 
+  let doc = Key.Arg.info ~doc:"This is the bgp identifier, i.e. ip address." ["local_id"] in
+  Key.(create "local_id" Arg.(opt string "172.19.0.3" doc))
+;;
+
+let local_port = 
+  let doc = Key.Arg.info ~doc:"This is the local bgp speaker's port." ["local_port"] in
+  Key.(create "local_port" Arg.(opt int 179 doc))
+;;
+
+let local_asn = 
+  let doc = Key.Arg.info ~doc:"This is the asn number for this bgp speaker." ["local_asn"] in
+  Key.(create "local_asn" Arg.(opt int 2 doc))
+;;
+
+let filename = 
+  let doc = Key.Arg.info ~doc:"This is the replay file." ["filename"] in
+  Key.(create "filename" Arg.(opt string "" doc))
+;;
+
+let quick = 
+  let doc = Key.Arg.info ~doc:"This is the replay mode." ["quick"] in
+  Key.(create "quick" Arg.(opt bool true doc))
+;;
+
+let total =
+  let doc = Key.Arg.info ~doc:"Total number of packages replayed" ["total"] in
+  Key.(create "total" Arg.(opt int 0 doc))
+;;
+
+
+let main = 
+  let keys = [
+    Key.abstract remote_id; 
+    Key.abstract remote_port; 
+    Key.abstract local_asn; 
+    Key.abstract local_port;
+    Key.abstract local_id;
+    Key.abstract filename; 
+    Key.abstract quick;
+    Key.abstract total;
+  ] in
+  foreign ~keys "Replay.Main" (stackv4 @-> job)
+;;
+
+let stack = generic_stackv4 default_network
+
+let () =
+  register "replay" [
+    main $ stack
+  ]
